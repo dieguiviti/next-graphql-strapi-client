374b827ba689ffc8c48be7579af01434ad257ca1
Diego Gallovich <dieguiviti@gmail.com>
2021-07-09 13:12:46 +0200

    Typescript + eslint + prettier + starter dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <link
+          rel='stylesheet'
+          href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
+          integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm'
+          crossOrigin='anonymous'
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
